e74199f4629dbd9ef50161d89907dba91a31e964
Kian-F <52603628+Kian-F@users.noreply.github.com>
2020-07-17 21:35:14 +1000

    filesCreated

---
+++ src/components/Booking.js
@@ -0,0 +1,19 @@
+
+import React, { Component } from 'react';
+
+
+class Booking extends Component {
+    constructor(){
+        super();
+    }
+    render(){
+        return(
+        <div>
+            <h1>Hi from Booking</h1>
+        </div>)
+
+        
+    }
+}
+
+export default Booking
