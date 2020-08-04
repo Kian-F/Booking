c895dfb13b0be9abb27e6b923624ba98943a6236
Kian-F <52603628+Kian-F@users.noreply.github.com>
2020-08-04 23:39:50 +1000

    date

--- src/components/Booking.js
+++ src/components/Booking.js
@@ -6,6 +6,7 @@ import Typography from '@material-ui/core/Typography';
 import Container from '@material-ui/core/Container';
 import Grid from '@material-ui/core/Grid';
 import DateFnsUtils from '@date-io/date-fns';
+import DatePicker from './DatePicker'
 import {
   MuiPickersUtilsProvider,
   KeyboardTimePicker,
@@ -22,6 +23,7 @@ class Booking extends Component {
         <React.Fragment>
             <CssBaseline/>
             <Container>
+            <DatePicker/>
             <Typography component="div" style={{ backgroundColor: '#cfe8fc', height: '50vh' }} />
             </Container>
         </React.Fragment>
