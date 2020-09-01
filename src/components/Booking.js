c95445f7a6583f4a61b6b531c52a8e9a0bdccc50
Kian-F <52603628+Kian-F@users.noreply.github.com>
2020-09-01 19:02:40 +1000

    grid

--- src/components/Booking.js
+++ src/components/Booking.js
@@ -7,7 +7,10 @@ import Grid from "@material-ui/core/Grid";
 import DateFnsUtils from "@date-io/date-fns";
 import DatePicker from "./DatePicker";
 import TimePickers from "./TimePickers";
-import NumberOfParty from './NumberOfParty'
+import NumberOfParty from "./NumberOfParty";
+
+import Button from "@material-ui/core/Button";
+
 import {
   MuiPickersUtilsProvider,
   KeyboardTimePicker,
@@ -19,21 +22,37 @@ class Booking extends Component {
   constructor() {
     super();
   }
+
   render() {
     return (
-      <React.Fragment>
-        <CssBaseline />
-        <Container>
-          <Typography
-            component="div"
-            style={{ backgroundColor: "#cfe8fc", height: "50vh" }}
-          >
-            <DatePicker />
-            <TimePickers />
-            <NumberOfParty/>
-          </Typography>
-        </Container>
-      </React.Fragment>
+      <form>
+        <React.Fragment>
+          <CssBaseline />
+          <Container>
+            <Typography
+              component="div"
+              style={{ backgroundColor: "#cfe8fc", height: "50vh" }}
+            >
+              <Grid container spacing={12}>
+                <Grid item xs={3}>
+                  <DatePicker />
+                  </Grid>
+                  <Grid item xs={3}>
+                  <TimePickers />
+                  </Grid>
+                  <Grid item xs={3}>
+                  <NumberOfParty />
+                  </Grid>
+                  <Grid item xs={3}>
+                  <Button variant="contained" color="primary">
+                    Submit
+                  </Button>
+                </Grid>
+              </Grid>
+            </Typography>
+          </Container>
+        </React.Fragment>
+      </form>
     );
   }
 }
