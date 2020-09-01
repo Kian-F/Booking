6bc04a9bb29d0fb9775b5674ef80d8279a0aeccb
Kian-F <52603628+Kian-F@users.noreply.github.com>
2020-09-01 12:23:32 +1000

    numberOfGuests

--- src/components/Booking.js
+++ src/components/Booking.js
@@ -1,41 +1,41 @@
-
-import 'date-fns';
-import React, { Component } from 'react';
-import CssBaseline from '@material-ui/core/CssBaseline';
-import Typography from '@material-ui/core/Typography';
-import Container from '@material-ui/core/Container';
-import Grid from '@material-ui/core/Grid';
-import DateFnsUtils from '@date-io/date-fns';
-import DatePicker from './DatePicker'
-import TimePickers from './TimePickers'
+import "date-fns";
+import React, { Component } from "react";
+import CssBaseline from "@material-ui/core/CssBaseline";
+import Typography from "@material-ui/core/Typography";
+import Container from "@material-ui/core/Container";
+import Grid from "@material-ui/core/Grid";
+import DateFnsUtils from "@date-io/date-fns";
+import DatePicker from "./DatePicker";
+import TimePickers from "./TimePickers";
+import NumberOfParty from './NumberOfParty'
 import {
   MuiPickersUtilsProvider,
   KeyboardTimePicker,
   KeyboardDatePicker,
   TimePicker,
-}  from '@material-ui/pickers';
-
+} from "@material-ui/pickers";
 
 class Booking extends Component {
-    constructor(){
-        super();
-    }
-    render(){
-        return(
-        <React.Fragment>
-            <CssBaseline/>
-            <Container>
-           
-        <Typography component="div" style={{ backgroundColor: '#cfe8fc', height: '50vh' }} >
-        <DatePicker/>
-            <TimePickers/>
-        </Typography>
-            </Container>
-        </React.Fragment>
-        )
-
-        
-    }
+  constructor() {
+    super();
+  }
+  render() {
+    return (
+      <React.Fragment>
+        <CssBaseline />
+        <Container>
+          <Typography
+            component="div"
+            style={{ backgroundColor: "#cfe8fc", height: "50vh" }}
+          >
+            <DatePicker />
+            <TimePickers />
+            <NumberOfParty/>
+          </Typography>
+        </Container>
+      </React.Fragment>
+    );
+  }
 }
 
-export default Booking
+export default Booking;
---
+++ src/components/NumberOfParty.js
@@ -0,0 +1,11 @@
+import React from 'react'
+import { TextField } from '@material-ui/core'
+
+
+
+
+export default function NumberOfParty() {
+    return(
+        <TextField id="outlined-basic" label="Number Of Guests" variant="outlined"/>
+    )
+} 
